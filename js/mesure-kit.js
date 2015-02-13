1c50bb23a3939760e4be2c96fd33657366893ff8
sgamel <sylvain.gamel@amadeus.com>
2015-02-13 18:02:54 +0100

    Small improvement on mesure-kit to provide a Bench object that will allow to run test and record all results to provide some reporting and average computation.

--- js/mesure-kit.js
+++ js/mesure-kit.js
@@ -57,6 +57,24 @@ Bench.prototype.saveResult = function(id, duration) {
     resultList.push(duration);
 };
 
+
+/** Register a new test.
+ * 
+ * @param {string} id
+ *  Identify both the test and a DOM element where the result shall be shown.
+ *  
+ * @param {function} setupFn
+ *  A closure to run before the nbLoop's runs of the test
+ *  
+ * @param {function} bodyFn
+ *  Closure implementing the test.
+ *  
+ * @param {function} cleanupFn
+ *  A closure to call after all runs of the test.
+ *  
+ * @returns {int}
+ *  Duration of the nbLoop runs in milliseconds.
+ */
 Bench.prototype.addTest = function(id, setupFn, bodyFn, cleanupFn) {
     this.registeredTest[ id ] = {
         setup: setupFn,
@@ -65,6 +83,17 @@ Bench.prototype.addTest = function(id, setupFn, bodyFn, cleanupFn) {
     };
 };
 
+
+
+/* Run identified test an nbLoop times.
+ * 
+ * @param {string} id
+ *  Identifier of the test (and also identifier of DOM element where run 
+ *  result will be output.
+ *  
+ * @returns {Number}
+ *  Duration of the test runs in milliseconds.
+ */
 Bench.prototype.runTest = function(id) {
     var theTest  = this.registeredTest[ id ];
     var duration = 0;
@@ -79,10 +108,34 @@ Bench.prototype.runTest = function(id) {
     return duration;
 };
 
-Bench.prototype.runTest = function(id) {};
 
+
+/** Run all the test registered using the addTest() method.
+ * 
+ * All tests will be run nbLoop times.
+ * 
+ * Timer results will be tracked in the object.
+ * 
+ * @returns {undefined}
+ */
+Bench.prototype.runAllTests = function() {
+    for ( var testId in this.registeredTest ) {
+        this.runTest(testId);
+    }
+};
+
+/** Run a global bench of tests.
+ * 
+ * @param {int} nbRun
+ *  Number of run for each tests. That mean each test will be run
+ *  nbLoop * nbRun.
+ *  All results will be tracked in the object.
+ * 
+ * @returns {undefined}
+ * 
+ */
 Bench.prototype.runBench = function(nbRun) {
     for ( var runIdx = 0 ; runIdx < nbRun ; ++runId ) {
-        this.runTest()
+        this.runAllTests();
     }
 };
